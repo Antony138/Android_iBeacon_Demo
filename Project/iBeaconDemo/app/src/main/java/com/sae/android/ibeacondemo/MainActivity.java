0379134c81a0fe6c0f49baa8b8954e2ed571c2a6
Antony <antony_huang@sae.com.hk>
2017-08-28 17:57:06 +0800

    实现蓝牙搜索。（not work）

--- Project/iBeaconDemo/app/src/main/java/com/sae/android/ibeacondemo/MainActivity.java
+++ Project/iBeaconDemo/app/src/main/java/com/sae/android/ibeacondemo/MainActivity.java
@@ -1,13 +1,253 @@
 package com.sae.android.ibeacondemo;
 
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@TargetApi(21)
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private int REQUEST_ENABLE_BT = 1;
+
+    private Handler mHandler;
+
+    private boolean mScanning;
+
+    // Stops canning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
+
+    private BluetoothLeScanner mLEScanner;
+
+    private ScanSettings settings;
+
+    private List<ScanFilter> filters;
+
+    private BluetoothGatt mGatt;
+
+    private TextView mTestTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate() called");
+
+        mHandler = new Handler();
+
+        // 如果硬件不支持蓝牙，提示使用者
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        mTestTextView = (TextView) findViewById(R.id.test_text_view);
+        mTestTextView.setText("这是手动添加的textView");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+        // Ensures Bluetooth is available on the device and it is enabled.
+        // If not, displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            // 如果没有打开蓝牙，要提示使用者打开
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            Log.d(TAG, "蓝牙没有打开，请打开手机蓝牙");
+        }
+        else {
+            // 如果蓝牙是打开的, 就开始搜索设备
+            if (Build.VERSION.SDK_INT >= 21) {
+                Log.d(TAG, "开始搜索设备");
+                // 如果API大于等于21，就配置BluetoothLeScanner
+                mLEScanner = mBluetoothAdapter.getBluetoothLeScanner();
+                settings = new ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build();
+                filters = new ArrayList<ScanFilter>();
+            }
+            scanLeDevice(true);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+        if (mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()) {
+            // 如果程序挂起，停止扫描
+            scanLeDevice(false);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+        // 程序推出, 关闭BluetoothGatt
+        if (mGatt == null) {
+            return;
+        }
+        mGatt.close();
+        mGatt = null;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            // Bluetooth not enabled
+            finish();
+            return;
+        }
+    }
+
+    // 扫描设备方法
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (Build.VERSION.SDK_INT < 21) {
+                        mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                    } else {
+                        mLEScanner.stopScan(mScanCallback);
+                    }
+                }
+            }, SCAN_PERIOD);
+
+            if (Build.VERSION.SDK_INT < 21) {
+                mBluetoothAdapter.startLeScan(mLeScanCallback);
+            } else {
+                mLEScanner.startScan(mScanCallback);
+                Log.d(TAG, "开始扫描");
+            }
+
+        } else {
+            if (Build.VERSION.SDK_INT < 21) {
+                mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            } else {
+                mLEScanner.stopScan(mScanCallback);
+            }
+        }
+    }
+
+    // Device scan callback
+    private ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            Log.d(TAG, "发现设备");
+
+            Log.i("扫描回调callbackType", String.valueOf(callbackType));
+            Log.i("扫描回调result", result.toString());
+
+            // 连接设备
+//            BluetoothDevice btDevice = result.getDevice();
+//            connectToDevice(btDevice);
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            for (ScanResult sr : results) {
+                Log.i("ScanResult - Results", sr.toString());
+            }
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            Log.e("Scan Failed", "Error Code: " + errorCode);
+        }
+        // Why semicolon here?
+        // 因为这个callback是一个属性，并不是一个方法！
+    };
+
+    // Device scan callback
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+//                    Log.d(TAG, "发现一个设备" + device + "rssi是:" + String.valueOf(rssi));
+                    Log.i("onLeScan", device.toString());
+
+                    // 连接设备
+//                    connectToDevice(device);
+                }
+            });
+        }
+    };
+
+    public void connectToDevice(BluetoothDevice device) {
+        if (mGatt == null) {
+            mGatt = device.connectGatt(this, false, gattCallbck);
+            // will stop scan after first device detection
+            scanLeDevice(false);
+        }
+    }
+
+    private final BluetoothGattCallback gattCallbck = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            Log.i("onConnectionStateChange", "Status: " + status);
+
+            switch (newState) {
+                case BluetoothProfile.STATE_CONNECTED:
+                    Log.i("gattCallback", "STATE_CONNECTED");
+                    // 连接成功，就去发现Services
+                    gatt.discoverServices();
+                    break;
+                case BluetoothProfile.STATE_DISCONNECTED:
+                    Log.e("gattCallback", "STATE_DISCONNECTED");
+                    break;
+                default:
+                    Log.e("gattCallback", "STATE_OTHER");
+            }
+        }
+    };
+
+    public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+        List<BluetoothGattService> services = gatt.getServices();
+        Log.i("onServicesDiscovered", services.toString());
+        gatt.readCharacteristic(services.get(1).getCharacteristics().get(0));
+    }
+
+    public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+        Log.i("onCharacteristicRead", characteristic.toString());
+        // 为什么要断开连接？
+        gatt.disconnect();
     }
 }
